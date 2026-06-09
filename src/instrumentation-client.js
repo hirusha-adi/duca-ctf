79cb836f6f2ca54aac1d08285723ee3e39cbbd6e
Hirusha Adikari <hello@hirusha.xyz>
2026-06-09 14:20:25 +1000

    sentry route bug fix

--- src/instrumentation-client.js
+++ src/instrumentation-client.js
@@ -1,3 +1,6 @@
+import * as Sentry from "@sentry/nextjs";
 import { initBugsink } from "./lib/bugsink";
 
 initBugsink();
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
